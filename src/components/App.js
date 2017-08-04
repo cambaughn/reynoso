5893048a74bf767a0475f23ae5b7c82156e63f49
Cameron Baughn <cam.baughn@gmail.com>
2017-08-04 00:06:30 -0700

    Add ScrollToTop functionality

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import {
   Route,
   Link,
   Switch
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 import NavBar from './NavBar/NavBar';
 import Footer from './Footer/Footer';
@@ -17,25 +17,29 @@ import Reels from './Reels/Reels';
 import About from './About/About';
 import Home from './Home';
 
+import ScrollToTop from './Router/ScrollToTop';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div style={styles.container}>
-          <NavBar />
+        <ScrollToTop>
+          <div style={styles.container}>
+            <NavBar />
 
-          <Switch>
-            <Route exact path={'/'} component={Home} />
-            <Route path={'/about'} component={About} />
-            {/* <Route path={'/resume'} component={Resume} /> */}
-            <Route path={'/reels'} component={Reels} />
-            <Route path={'/contact'} component={Contact} />
-          </Switch>
+            <Switch>
+              <Route exact path={'/'} component={Home} />
+              <Route path={'/about'} component={About} />
+              {/* <Route path={'/resume'} component={Resume} /> */}
+              <Route path={'/reels'} component={Reels} />
+              <Route path={'/contact'} component={Contact} />
+            </Switch>
 
-          <Footer />
+            <Footer />
 
-        </div>
+          </div>
+        </ScrollToTop>
       </Router>
     );
   }
---
+++ src/components/Router/ScrollToTop.js
@@ -0,0 +1,20 @@
+/*eslint no-use-before-define: "off"*/
+/*eslint no-unused-vars: "off"*/
+/*eslint-env es6*/
+
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
+export default withRouter(ScrollToTop);
