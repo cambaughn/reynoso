7f0333fd848063dcedec5c863927266ec5b0d43d
Cameron Baughn <cam.baughn@gmail.com>
2017-08-20 14:54:01 -0700

    Organize templates

--- src/components/Template/ClassTemplate.js
+++ src/components/Template/ClassTemplate.js
@@ -6,35 +6,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
-const Template = () => {
-  return (
-    <div style={styles.container}>
-
-    </div>
-  )
-}
-
-
-const styles = {
-  // CONTAINER ---------------------
-
-  container: {
-
-  },
-}
-
-export default Template;
-
-// ---------- OR ----------
-
-/*eslint no-use-before-define: "off"*/
-/*eslint no-unused-vars: "off"*/
-/*eslint-env es6*/
-
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
 class Template extends Component {
   constructor(props) {
     super(props);
